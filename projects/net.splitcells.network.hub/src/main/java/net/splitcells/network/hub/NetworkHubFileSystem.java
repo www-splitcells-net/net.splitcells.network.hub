fb8bd2512abf35256bf6b913b2088bca78d72a9e
Mārtiņš Avots <martins.avots@splitcells.net>
2025-02-01 05:50:14 +0100

    Move all system config into cell #42
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.network.hub/src/main/java/net/splitcells/network/hub/NetworkHubFileSystem.java
+++ projects/net.splitcells.network.hub/src/main/java/net/splitcells/network/hub/NetworkHubFileSystem.java
@@ -15,12 +15,12 @@
  */
 package net.splitcells.network.hub;
 
-import net.splitcells.dem.environment.config.framework.OptionI;
+import net.splitcells.dem.environment.config.framework.OptionImpl;
 import net.splitcells.dem.resource.FileSystemView;
 
 import static net.splitcells.dem.resource.FileSystemViaClassResources.fileSystemViaClassResources;
 
-public class NetworkHubFileSystem extends OptionI<FileSystemView> {
+public class NetworkHubFileSystem extends OptionImpl<FileSystemView> {
     public NetworkHubFileSystem() {
         super(() -> fileSystemViaClassResources(NetworkHubFileSystem.class, "net.splitcells", "network.hub"));
     }
