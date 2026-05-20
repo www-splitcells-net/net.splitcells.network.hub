95868c41e0bee043b67806466188fd677be8ff59
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-20 21:57:19 +0200

    Committed by "splitcells" on "splitcells-XPS-15-9570".

--- projects/net.splitcells.network.hub/src/main/java/net/splitcells/network/hub/NetworkHubFileSystem.java
+++ projects/net.splitcells.network.hub/src/main/java/net/splitcells/network/hub/NetworkHubFileSystem.java
@@ -16,12 +16,19 @@
 package net.splitcells.network.hub;
 
 import net.splitcells.dem.environment.config.framework.OptionImpl;
+import net.splitcells.dem.lang.tree.Tree;
 import net.splitcells.dem.resource.FileSystemView;
 
+import java.util.Optional;
+
+import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.resource.FileSystemViaClassResources.fileSystemViaClassResources;
 
 public class NetworkHubFileSystem extends OptionImpl<FileSystemView> {
     public NetworkHubFileSystem() {
         super(() -> fileSystemViaClassResources(NetworkHubFileSystem.class, "net.splitcells", "network.hub"));
+    }
+    @Override public Optional<Tree> serialize(FileSystemView currentValue) {
+        return Optional.of(tree(currentValue.toString()));
     }
 }
